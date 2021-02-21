6ca7a1615bfccd837ce2994e1782a0a0a1b4264f
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-20 23:09:42 -0700

    Poppins font, systemuioverlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:radio_app/pages/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AI Radio App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+        title: 'AI Radio App',
+        theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
   }
 }
